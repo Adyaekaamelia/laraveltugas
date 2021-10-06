8621f38f97f6493456220c69d616dd7aac5925bc
Adya <adyka013@gmail.com>
2021-10-06 19:40:33 +0700

    tugas blade

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+})->name('index');
+Route::get('about', function () {
+     return view('layouts.about');
+ })->name('about');
+ Route::get('post', function () {
+     return view('layouts.post');
+ })->name('post');
+ Route::get('contact', function () {
+     return view('layouts.contact');
+ })->name('contact');
+
+
+
+
+
